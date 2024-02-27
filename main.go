124832218afb1205f09bcc2e8c514a5c73b8457f
Aryan Agarwal <aryankush025@gmail.com>
2024-02-27 22:42:33 +0530

    Add newCard function and update main function

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	card := newCard()
+	cards := []string{"new card", newCard()}
+
+	cards = append(cards, "Six of Spades")
+
+	for i, cards := range cards {
+		fmt.Println(i, cards)
+	}
+
+	fmt.Println(cards)
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
