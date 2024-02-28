0186613499ab9b3136e5077c3793cb6f89e0b020
Aryan Agarwal <aryankush025@gmail.com>
2024-02-28 12:18:56 +0530

    Add newDeck and deal functions to deck.go, update main.go to use newDeck and deal functions

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	cards := deck{"new card", newCard()}
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
 
-	cards = append(cards, "Six of Spades")
-
-	cards.print()
-
-	fmt.Println(cards)
-	fmt.Println(card)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
 }
