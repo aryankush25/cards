14ca249ed4c6dde12d9a9de1ca0bc0dc08444b78
Aryan Agarwal <aryankush025@gmail.com>
2024-03-18 23:06:38 +0530

    Odd even problem

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// cards := newDeck()
 	// cards.print()
@@ -16,8 +20,18 @@ func main() {
 	// cards.print()
 	// fmt.Println(cards.toString(", "))
 
-	cards := newDeck()
-	cards.print()
-	cards.shuffle()
-	cards.print()
+	// cards := newDeck()
+	// cards.print()
+	// cards.shuffle()
+	// cards.print()
+
+	numbers := []int{0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, number := range numbers {
+		if number % 2 == 0 {
+			fmt.Println(number, "is even")
+		} else {
+			fmt.Println(number, "is odd")
+		}
+	}
 }
