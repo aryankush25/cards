15a2c8c5abc3638775bea54d2f52e14cda83aec2
Aryan Agarwal <aryankush025@gmail.com>
2024-03-18 22:56:17 +0530

    Tests for TestSaveToDeckAndNewDeckFromFile added

--- deck_test.go
+++ deck_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"os"
 )
 
 func TestNewDeck(t *testing.T) {
@@ -18,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if deck.toString(",") != loadedDeck.toString(",") {
+		t.Errorf("Expected to be same cards in both decks")
+	}
+
+	os.Remove("_decktesting")
 }
